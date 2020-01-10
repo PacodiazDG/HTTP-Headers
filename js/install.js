e3dfab638ee8b2899ec9c43104ac132953bf2139
paco diaz <pacoxdja@hotmail.es>
2020-01-10 01:39:22 -0600

    _0x

--- js/install.js
+++ js/install.js
@@ -6,7 +6,7 @@ localStorage.setItem("Debugging4",false);
 localStorage.setItem("Debugging5",false);
 localStorage.setItem("Debugging6",false);
 localStorage.setItem("Debugging7",false);
-localStorage.setItem("varcommand","bgfbfgbfgbfgb");
+localStorage.setItem("varcommand","_0x");
 localStorage.setItem("waflevel",1);
 localStorage.setItem("status",false);
 localStorage.setItem("keyshodan","");
