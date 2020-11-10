6bd4fe09461cb1b0c1d2aea39e1c6b551b1ae595
Francisco Javier Diaz De Leon <pacoxdja@hotmail.es>
2020-11-10 00:43:27 -0600

    Remove decode menu

--- js/contextMenus/contextMenus.js
+++ js/contextMenus/contextMenus.js
@@ -1,13 +1,4 @@
 chrome.contextMenus.create({
-  "title": "Decoder",
-  "contexts": ["browser_action"],
-  "onclick": (info, tab) => {
-    chrome.tabs.create({
-      url: "decoder.html?q="
-    })
-  }
-});
-chrome.contextMenus.create({
   "title": "Change URL parameters",
   "contexts": ["browser_action"],
   "onclick": (info, tab) => {
