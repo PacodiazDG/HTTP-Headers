bce5fccdb3ddbfb26c4558107cac2aeb3b9b869c
Francisco Javier DL <pacoxdja@hotmail.es>
2020-12-19 23:17:41 -0700

    Add  remove Listener in UserAgent

--- js/background/browser-fingerprint/user-agent.js
+++ js/background/browser-fingerprint/user-agent.js
@@ -1,23 +1,28 @@
-function Callbackuseragent(details) {
+function Callbackuseragent(Header) {
 	var UserAgent = localStorage.getItem("User-Agent");
-if (UserAgent==null){
-	return;
-}
-	for (var i = 0, l = details.requestHeaders.length; i < l; ++i) {
-			if (details.requestHeaders[i].name !== 'User-Agent') {
-				continue;
-			}
-			details.requestHeaders[i].value = UserAgent;
-			break;
-		}
-		return {
-			requestHeaders: details.requestHeaders
-		};
+	if (UserAgent == null) {
+		return;
 	}
-export function UserAgent() {
-		chrome.webRequest.onBeforeSendHeaders.addListener(
-			Callbackuseragent,
-			{ urls: ["<all_urls>"] },
-			["blocking", "requestHeaders"]
+	if((localStorage.getItem("UserAgent1"))==="false"||(localStorage.getItem("status") === "false")){
+		chrome.webRequest.onBeforeSendHeaders.removeListener(
+			Callbackuseragent
 		);
 	}
+	for (var i = 0, l = Header.requestHeaders.length; i < l; ++i) {
+		if (Header.requestHeaders[i].name !== 'User-Agent') {
+			continue;
+		}
+		Header.requestHeaders[i].value = UserAgent;
+		break;
+	}
+	return {
+		requestHeaders: Header.requestHeaders
+	};
+}
+export function UserAgent() {
+	chrome.webRequest.onBeforeSendHeaders.addListener(
+		Callbackuseragent,
+		{ urls: ["<all_urls>"] },
+		["blocking", "requestHeaders"]
+	);
+}
