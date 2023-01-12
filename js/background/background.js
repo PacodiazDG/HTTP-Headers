1b44099f287423783451a212bad086714ee5ad68
Francisco Javier Diaz <pacodiazl@outlook.com>
2023-01-12 17:24:48 -0600

    Update

--- js/background/background.js
+++ js/background/background.js
@@ -1,12 +1,16 @@
 "use strict";
-import { isValidURL } from "./module/module.js";
+import {
+  isValidURL
+} from './module/module.js';
 import {
   enableContextMenus,
   Debugging7control1,
   Debugging7control2,
   EnablesDump1,
-} from "./module/inyeccion.js";
-import { UserAgent } from "./inject/user-agent.js";
+} from './module/inyeccion.js';
+import {
+  UserAgent
+} from './inject/user-agent.js';
 
 //Esta funcion Cambia el fingerPrinting del navegador a nivel javascript
 /*
@@ -20,35 +24,29 @@ chrome.runtime.onMessage.addListener((msg, sender_info, Reply) => {
 });
 */
 
-//Escucha todos los eventos
+//Escucha todos los eventos 
+
+
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
   if (!isValidURL(tab.url)) {
     return;
   }
+  
   var executeScript = (codes) => {
-    "use strict";
-    chrome.scripting.executeScript(
-      {
-        injectImmediately: true,
-        target: { tabId: tabId, allFrames: true },
-        func: (codes) => {
-          coder(codes);
-        },
-        args: [codes],
-      },
-      () => {}
-    );
+    'use strict';
+    console.log(tabId)
+    chrome.scripting.executeScript({
+      injectImmediately:true,
+      target:{tabId: tabId,allFrames: true},
+      func: (codes)=>{ coder(codes)},
+      args: [codes]
+    },
+    () => {});
   };
-  if ((await chrome.storage.local.get(["status"])).status === true) {
-    chrome.action.setBadgeText({ text: "Work!" });
-    if (changeInfo.status === "complete") {
-      const keys = await chrome.storage.local.get([
-        "Debugging1",
-        "Debugging2",
-        "UserAgent1",
-        "Debugging3",
-        "Debugging4",
-      ]);
+  if ((await chrome.storage.local.get(['status'])).status === true) {
+    chrome.action.setBadgeText({ text: "Work!", });
+    if (changeInfo.status === 'complete') {
+      const keys = await chrome.storage.local.get(['Debugging1', 'Debugging2', 'UserAgent1', 'Debugging3', 'Debugging4']);
       if (keys.Debugging1 == true) {
         executeScript(
           `var highestTimeoutId = setTimeout(";");
@@ -74,8 +72,10 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
       }
     }
   } else {
-    chrome.action.setBadgeText({
-      text: "",
-    });
+    chrome.action.setBadgeText(
+      {
+        text: "",
+      }
+    );
   }
 });
